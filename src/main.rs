e8aa2f51ee78d2b32a9697b9087397f22cccbce8
Ghulam Abbas <ghulamabbas@Ghulams-MacBook-Pro.local>
2022-02-07 09:31:53 -0500

    added method to read input from user and print it to console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io; 
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Start");
+
+    let mut command = String::new(); 
+
+    io::stdin()
+        .read_line(&mut command)
+        .expect("Failed to read input"); 
+
+    println!("> {}", command);
 }
