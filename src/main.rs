6d3d031498b95a028e221e093a0d4f7a9056ba22
Ghulam Afzal <inferno2960@gmail.com>
2022-03-02 15:47:48 -0500

    seperated out the ability to run a command into its own function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use std::io::{self, Write}; 
-use std::process::Command; 
+use std::process::Command;
+
+// func to run the command that was passed in 
+fn perfrom_command(command: &String) {
+    
+    let mut child = Command::new(command)
+        .spawn()
+        .expect("not a valid command");
+
+   let s_code = child.wait()
+       .expect("Failed to wait on child process.");
+
+   // assert that the command succeded 
+   assert!(s_code.success());
+}
+
 
 fn main() {
     
@@ -9,7 +24,7 @@ fn main() {
         print!("> ");
         io::stdout().flush().unwrap();
 
-        let mut user_input  = String::new();
+        let mut user_input = String::new();
 
         io::stdin().read_line(&mut user_input).unwrap();
 
@@ -20,12 +35,9 @@ fn main() {
             break 
         }
 
-        let mut child = Command::new(command)
-            .spawn()
-            .unwrap();
-
-        // to make sure that only one command is accecpted at a time
-        child.wait(); 
+        // call perfrom_cammand() with the correct params;
+        let command_string = command.to_string();    
+        perfrom_command(&command_string);
 
     }
 }
