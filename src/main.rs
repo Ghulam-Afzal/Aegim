8a02c071218c77377654f43c0610c35c3b619e31
Ghulam Abbas <inferno2960@gmail.com>
2022-05-20 09:51:19 -0400

    changed the file structure

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,9 @@
 use std::io::{self, Write}; 
-use std::process::Command;
 
-// func to run the command that was passed in 
-fn _perfrom_command(command: String, args: Vec<&str>) {
-    
-    let mut child = Command::new(command)
-        .args(args)
-        .spawn()
-        .expect("not a valid command");
+#[path = "helpers/print_type.rs"] mod print_type;
 
-   let s_code = child.wait()
-       .expect("Failed to wait on child process.");
-
-   // assert that the command succeded 
-   assert!(s_code.success());
-}
-
-fn _print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
-
-
-// parse the command and split it into parts 
-fn seperate_command(command: &String) -> (String, Vec<&str>) {
-
-    // split the array by whitespace and than collet into &str array
-    let mut parts: Vec<&str> = command.trim().split_whitespace().collect();
-
-    // first index of parts == command
-    let cmd = parts[0];
-
-    // remove the first index and than assign the array to args
-    parts.retain(|&x| x != cmd);
-    let args = parts;
-    
-    return (cmd.to_string(), args);
-    
-}
+mod perform_command;
+mod seperate_command;
 
 fn main() {
     
@@ -62,9 +29,9 @@ fn main() {
 
         // call perfrom_cammand() with the correct params;
         let command_string = command.to_string();    
-        (cmd, args) = seperate_command(&command_string);
+        (cmd, args) = seperate_command::seperate_command(&command_string);
 
-        _perfrom_command(cmd, args);
+        perform_command::perform_command(cmd, args);
 
     }
 }
---
+++ src/perform_command.rs
@@ -0,0 +1,16 @@
+use std::process::Command;
+
+// func to run the command that was passed in 
+pub fn perform_command(command: String, args: Vec<&str>) {
+    
+    let mut child = Command::new(command)
+        .args(args)
+        .spawn()
+        .expect("not a valid command");
+
+   let s_code = child.wait()
+       .expect("Failed to wait on child process.");
+
+   // assert that the command succeded 
+   assert!(s_code.success());
+}
