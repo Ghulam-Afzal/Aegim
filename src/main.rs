0dfe221086c6fad9f48f16892e6f429a3a672d6a
Ghulam Abbas <inferno2960@gmail.com>
2022-05-19 22:13:44 -0400

    added a function to seperate the command from the args and run that command with the args; added funtion to print the type of the variables

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use std::io::{self, Write};
 use std::process::Command;
 
 // func to run the command that was passed in 
-fn perfrom_command(command: &String) {
+fn _perfrom_command(command: String, args: Vec<&str>) {
     
     let mut child = Command::new(command)
+        .args(args)
         .spawn()
         .expect("not a valid command");
 
@@ -15,6 +16,27 @@ fn perfrom_command(command: &String) {
    assert!(s_code.success());
 }
 
+fn _print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+
+// parse the command and split it into parts 
+fn seperate_command(command: &String) -> (String, Vec<&str>) {
+
+    // split the array by whitespace and than collet into &str array
+    let mut parts: Vec<&str> = command.trim().split_whitespace().collect();
+
+    // first index of parts == command
+    let cmd = parts[0];
+
+    // remove the first index and than assign the array to args
+    parts.retain(|&x| x != cmd);
+    let args = parts;
+    
+    return (cmd.to_string(), args);
+    
+}
 
 fn main() {
     
@@ -35,9 +57,14 @@ fn main() {
             break 
         }
 
+        let mut cmd = String::new();
+        let mut args = Vec::new();
+
         // call perfrom_cammand() with the correct params;
         let command_string = command.to_string();    
-        perfrom_command(&command_string);
+        (cmd, args) = seperate_command(&command_string);
+
+        _perfrom_command(cmd, args);
 
     }
 }
