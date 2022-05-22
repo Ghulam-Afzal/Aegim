faba3dbc1090dd93e4c68f4a3a638f2ba6cb1b94
Ghulam Abbas <inferno2960@gmail.com>
2022-05-22 16:52:52 -0400

    shell now creates a history file and appends all user inputs to it

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, Write}; 
+use std::fs::OpenOptions;
 
 #[path = "helpers/print_type.rs"] mod print_type;
 
@@ -7,6 +8,9 @@ mod seperate_command;
 
 fn main() {
     
+    // create a fall if not exists to store the command history
+    let mut command_history_file = OpenOptions::new().read(true).append(true).create(true).open("aegim_history.txt").unwrap();
+
     // main loop for the program 
     loop {
 
@@ -19,12 +23,19 @@ fn main() {
 
         let command = user_input.trim(); 
         
+        // wrtie the comamnd to the history file
+        command_history_file.write_all(&command.as_bytes());
+        command_history_file.write_all("\n".as_bytes());
+
         // if the command that was entered was exit that break out of the loop 
         if command == "exit" { 
             break 
         }
 
+        #[allow(unused)]
         let mut cmd = String::new();
+        
+        #[allow(unused)]
         let mut args = Vec::new();
 
         // call perfrom_cammand() with the correct params;
